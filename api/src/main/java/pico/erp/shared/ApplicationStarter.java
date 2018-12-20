deaf1530c1add207c5293f66bf267f4573665b57
Ethan <kkojaeh@gmail.com>
2018-12-21 08:39:48 +0900

    코드 복원

--- api/src/main/java/pico/erp/shared/ApplicationStarter.java
+++ api/src/main/java/pico/erp/shared/ApplicationStarter.java
@@ -3,31 +3,20 @@ package pico.erp.shared;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.val;
 
 public interface ApplicationStarter {
 
   static List<ApplicationStarter> sort(List<ApplicationStarter> starters) {
     val result = new LinkedList<ApplicationStarter>();
-    val targets = new LinkedList<ApplicationStarter>();
-    targets.addAll(starters);
+    val targets = new LinkedList<ApplicationStarter>(starters);
 
     outer:
     while (!targets.isEmpty()) {
       for (ApplicationStarter target : targets) {
-        System.out.println(target.getId().getValue() + " : " + target.getDependencies().stream()
-          .map(id -> id.getValue()).collect(
-            Collectors.toSet()
-          ));
-        System.out.println("targets : " + targets.stream()
-          .map(t -> t.getId().getValue()).collect(
-            Collectors.toSet()
-          ));
-        val hasDependency = targets.stream()
-          .map(ApplicationStarter::getId)
-          .filter(id -> target.getDependencies().contains(id))
-          .count() > 0;
+        val hasDependency = target.getDependencies()
+          .stream()
+          .anyMatch(dependency -> targets.stream().anyMatch(t -> t.getId().equals(dependency)));
         if (!hasDependency) {
           targets.remove(target);
           result.add(target);
