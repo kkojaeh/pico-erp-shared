bb1a11270b222adf3ebac087816cc79af68074c2
Ethan <kkojaeh@gmail.com>
2019-03-19 08:55:45 +0900

    단위에 precision 추가

--- api/src/main/java/pico/erp/shared/data/UnitKind.java
+++ api/src/main/java/pico/erp/shared/data/UnitKind.java
@@ -1,35 +1,36 @@
 package pico.erp.shared.data;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
 public enum UnitKind implements LocalizedNameable {
 
-  EA(UnitTypeKind.QUANTITY),
-  SHEET(UnitTypeKind.QUANTITY),
-  MM(UnitTypeKind.LENGTH),
-  CM(UnitTypeKind.LENGTH),
-  M(UnitTypeKind.LENGTH),
-  KM(UnitTypeKind.LENGTH),
+  EA(UnitTypeKind.QUANTITY, 0),
+  SHEET(UnitTypeKind.QUANTITY, 0),
+  MM(UnitTypeKind.LENGTH, 0),
+  CM(UnitTypeKind.LENGTH, 0),
+  M(UnitTypeKind.LENGTH, 0),
+  KM(UnitTypeKind.LENGTH, 0),
 
-  M2(UnitTypeKind.AREA),
+  M2(UnitTypeKind.AREA, 0),
 
-  MG(UnitTypeKind.WEIGHT),
-  G(UnitTypeKind.WEIGHT),
-  KG(UnitTypeKind.WEIGHT),
-  T(UnitTypeKind.WEIGHT),
+  MG(UnitTypeKind.WEIGHT, 0),
+  G(UnitTypeKind.WEIGHT, 0),
+  KG(UnitTypeKind.WEIGHT, 0),
+  T(UnitTypeKind.WEIGHT, 0),
 
-  CC(UnitTypeKind.VOLUME),
-  ML(UnitTypeKind.VOLUME),
-  DL(UnitTypeKind.VOLUME),
-  L(UnitTypeKind.VOLUME),
-  CM3(UnitTypeKind.VOLUME),
-  M3(UnitTypeKind.VOLUME);
+  CC(UnitTypeKind.VOLUME, 0),
+  ML(UnitTypeKind.VOLUME, 0),
+  DL(UnitTypeKind.VOLUME, 0),
+  L(UnitTypeKind.VOLUME, 0),
+  CM3(UnitTypeKind.VOLUME, 0),
+  M3(UnitTypeKind.VOLUME, 0);
 
+  @Getter
   private final UnitTypeKind type;
 
-  public UnitTypeKind type() {
-    return type;
-  }
+  @Getter
+  private final int precision;
 
 }
