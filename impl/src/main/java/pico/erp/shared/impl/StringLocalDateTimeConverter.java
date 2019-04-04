fb1f7c3295feccc61a7c179a9774788feed9276a
Ethan <kkojaeh@gmail.com>
2019-04-04 17:24:28 +0900

    localdatetime 컨버트 변경

--- impl/src/main/java/pico/erp/shared/impl/StringLocalDateTimeConverter.java
+++ impl/src/main/java/pico/erp/shared/impl/StringLocalDateTimeConverter.java
@@ -1,6 +1,9 @@
 package pico.erp.shared.impl;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import org.springframework.core.convert.converter.Converter;
 
 public class StringLocalDateTimeConverter implements Converter<String, LocalDateTime> {
@@ -10,6 +13,14 @@ public class StringLocalDateTimeConverter implements Converter<String, LocalDate
     if (source == null || source.isEmpty()) {
       return null;
     }
+    if (source.length() > 10 && source.charAt(10) == 'T') {
+      if (source.endsWith("Z")) {
+        return LocalDateTime.ofInstant(Instant.parse(source), ZoneId.systemDefault());
+      } else {
+        return LocalDateTime.parse(source, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+      }
+    }
     return LocalDateTime.parse(source);
   }
+
 }
